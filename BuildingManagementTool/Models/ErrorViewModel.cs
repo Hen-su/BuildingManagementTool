ec1da634bf0b17e642cc307e6eb3cd5b2261b7f2
Hen-su <henleysu@outlook.com>
2024-08-07 04:56:36 +1200

    Add project files.

---
+++ BuildingManagementTool/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BuildingManagementTool.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ BuildingManagementTool/Services/BlobService.cs
@@ -0,0 +1,13 @@
+﻿using Azure.Storage.Blobs;
+
+namespace BuildingManagementTool.Services
+{
+    public class BlobService
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+        public BlobService(BlobServiceClient blobServiceClient)
+        {
+            _blobServiceClient = blobServiceClient;
+        }
+    }
+}
