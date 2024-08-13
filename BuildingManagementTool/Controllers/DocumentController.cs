b3dc599f1d6e91b782e60858c774e53f28c954fb
aditya_7 <adityasharma162002@gmail.com>
2024-08-14 00:03:25 +1200

    added the side preview menu

--- BuildingManagementTool/Controllers/DocumentController.cs
+++ BuildingManagementTool/Controllers/DocumentController.cs
@@ -1,28 +1,37 @@
 ﻿using BuildingManagementTool.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.IO.Pipelines;
+using System.Linq;
 
 namespace BuildingManagementTool.Controllers
 {
     public class DocumentController : Controller
     {
         private readonly IDocumentRepository _fileRepository;
+
         public DocumentController(IDocumentRepository fileRepository)
         {
             _fileRepository = fileRepository;
         }
-        public IActionResult Index()
+
+        public IActionResult Index(int? selectedDocumentId)
         {
             var documents = _fileRepository.AllDocuments;
-            return View(documents);
+            Document selectedDocument = null; //no document selected 
+
+            // only selection of documents on clicks
+            if (selectedDocumentId.HasValue)
+            {// if selectedDocumentId is not null it will match the documentId's of all documents to the selected one and pass it to selectedDocument
+                selectedDocument = documents.FirstOrDefault(d => d.DocumentId == selectedDocumentId.Value);
+            }
+
+            // Pass documents and selectedDocument to the view 
+            return View((documents, selectedDocument));
         }
+
 
         public IActionResult UploadFormPartial()
         {
             return PartialView("_UploadForm");
         }
-     
-
-
     }
 }
