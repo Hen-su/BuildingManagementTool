eed92e26a551a7cd05daa64c3e8eb54fa8924a2f
Hen-su <henleysu@outlook.com>
2024-09-03 21:45:19 +1200

    Fixed merge errors

--- BuildingManagementTool/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BuildingManagementTool/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -145,14 +145,6 @@ namespace BuildingManagementTool.Areas.Identity.Pages.Account
                         values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
                         protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                    await _emailSender.SendEmailAsync(
-                        Input.Email,
-                        "Confirm your email",
-                        htmlContent);
-
                     // Assign the user to the selected role
                     if (!await _userManager.IsInRoleAsync(user, Input.Role))
                     {
