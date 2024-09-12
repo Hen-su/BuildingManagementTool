5bc533c9d0d4835ae2ee37b1dd51709dc4108a9f
aditya_7 <adityasharma162002@gmail.com>
2024-09-12 16:34:53 +1200

    Category boxes color picker functionality implemented

---
+++ BuildingManagementTool/Migrations/20240911124251_addColorToPropertyCategory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BuildingManagementTool.Migrations
+{
+    /// <inheritdoc />
+    public partial class addColorToPropertyCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "PropertyCategories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "PropertyCategories");
+        }
+    }
+}
